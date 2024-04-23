2e01b0efad412fb9e421c264ac15453885abcca0
Gokhan Kus <gkus1998@hotmail.com>
2024-04-23 13:14:48 +0300

    Add project files.

---
+++ OOP/Classes/Calisan.cs
@@ -0,0 +1,21 @@
+﻿using OOP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Classes
+{
+    public abstract class Calisan : ICalisan
+    {
+        public string Adi{ get; set; }
+        public string Soyadi{ get; set; }
+        public string FullIsmi => $"{Adi} {Soyadi}";
+        public string Departmani{ get; set; }
+        public int CalisaninMaasi(int maas)
+        {
+            return maas;
+        }
+    }
+}
