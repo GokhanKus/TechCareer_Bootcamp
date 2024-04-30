bca7a5098ce70e80e5a9fcd9a7e2c8ff347c225c
Gokhan Kus <gkus1998@hotmail.com>
2024-04-30 13:21:05 +0300

    combobox'a db'deki odemeYontemleri getirildi

---
+++ OdemeFormu/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace OdemeFormu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.cmbOdemeTipi = new System.Windows.Forms.ComboBox();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.btnOdemeYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Odeme Tipi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tutar";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(186, 169);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(41, 13);
+            this.lblSonuc.TabIndex = 0;
+            this.lblSonuc.Text = "Sonuc:";
+            // 
+            // cmbOdemeTipi
+            // 
+            this.cmbOdemeTipi.FormattingEnabled = true;
+            this.cmbOdemeTipi.Location = new System.Drawing.Point(189, 132);
+            this.cmbOdemeTipi.Name = "cmbOdemeTipi";
+            this.cmbOdemeTipi.Size = new System.Drawing.Size(121, 21);
+            this.cmbOdemeTipi.TabIndex = 1;
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(345, 132);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(100, 20);
+            this.txtTutar.TabIndex = 2;
+            // 
+            // btnOdemeYap
+            // 
+            this.btnOdemeYap.Location = new System.Drawing.Point(24, 100);
+            this.btnOdemeYap.Name = "btnOdemeYap";
+            this.btnOdemeYap.Size = new System.Drawing.Size(138, 52);
+            this.btnOdemeYap.TabIndex = 3;
+            this.btnOdemeYap.Text = "Odeme Yap";
+            this.btnOdemeYap.UseVisualStyleBackColor = true;
+            this.btnOdemeYap.Click += new System.EventHandler(this.btnOdemeYap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 257);
+            this.Controls.Add(this.btnOdemeYap);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.cmbOdemeTipi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.ComboBox cmbOdemeTipi;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Button btnOdemeYap;
+    }
+}
+
---
+++ OdemeFormu/Form1.cs
@@ -0,0 +1,71 @@
+﻿using OdemeFormu.OdemeYontemleri;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdemeFormu
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=TechCareer; Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOdemeYap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OdemeYontemleriniGetir();
+        }
+
+        private void OdemeYontemleriniGetir()
+        {
+            List<OdemeTipi> odemeTipleri = new List<OdemeTipi> { new OdemeTipi { ClassName = "", DisplayName = "Odeme Tipi Seciniz" } };
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "select * from OdemeYontemi";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string className = (string)reader["ClassName"];
+                        string displayName = (string)reader["DisplayName"];
+                        odemeTipleri.Add(new OdemeTipi
+                        {
+                            ClassName = className,
+                            DisplayName = displayName
+                        });
+
+                    }
+                    cmbOdemeTipi.DataSource = odemeTipleri;
+                    cmbOdemeTipi.DisplayMember = "DisplayName";
+                    cmbOdemeTipi.ValueMember = "ClassName";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("bir hata meydana geldi: " + ex.Message);
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
